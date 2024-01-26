bbdaa700fb9c815913e7f94dd644ca02ec27494d
Jeanpk12 <jeanolivera123@gmail.com>
2024-01-26 12:21:10 -0300

    App financeiro

---
+++ src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+height: 150px;
+text-align: center;
+background-color: #0b1528;
+`;
+
+export const Header = styled.h1`
+    text-transform: uppercase;
+    text-shadow: 2px 4px 2px rgba(0,0,0,0.6);
+`;
+
+export const Title = styled.div`
+padding-top: 20px;
+color: #fff;
+`
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,16 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    font-family: 'Poppins', sans-serif;
+    background-color: #060b14;
+}
+`;
+
+export default GlobalStyles
